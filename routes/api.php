ef060fa28dd56321398ff62513bed9a8be98716a
sikirumoshood <sikirumoshood@gmail.com>
2019-06-17 18:32:54 +0100

    Initial setup

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Expenses;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:api')->get('/expenses',['headers'=>['Accept' => 'application/json']], function(Request $request){
+  $exps = Expenses::all();
+  return response()->json($exps);
 });
